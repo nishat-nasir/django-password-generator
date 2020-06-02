73e861bb4bcc5e775eda2806ae743277c7a43406
nishat-nasir <nishatnasir00@gmail.com>
2020-06-02 18:03:14 +0900

    updated the html and style

--- generator/views.py
+++ generator/views.py
@@ -1,9 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 import random
+
 # Create your views here.
+
 def home(request):
     return render(request, 'generator/home.html')
+
+
+def about(request):
+    return render(request, 'generator/about.html')
+
 
 def password(request):
 
@@ -23,6 +30,3 @@ def password(request):
         thepassword += random.choice(characters)
 
     return render(request, 'generator/password.html', {'password':thepassword})
-
-def about(request):
-    return render(request, 'generator/about.html')
